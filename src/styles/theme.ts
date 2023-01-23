2b7dbfdb26c3d2475270a57b75bf7034423cb208
Rian Ernesto <rianernesto9@gmail.com>
2023-01-23 15:24:12 -0300

    :tada: initial: Inicialização e configuração do repositório

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+    colors: {
+        darkBlue: '#1F3DAD',
+        purple: '#3D07B7',
+        blue: '#009ADB',
+        white: '#D9D9D9',
+        darkerBlue: '#161957',
+        black: '#010101'
+    }
+}
+
+export default theme
