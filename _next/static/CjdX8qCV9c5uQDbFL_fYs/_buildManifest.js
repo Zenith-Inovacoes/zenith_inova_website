570f064c6965ca904a2d8f6eb88a84c6461f91c9
RianErnesto <91086261+RianErnesto@users.noreply.github.com>
2023-02-02 19:55:56 +0000

    Build: (822ec49) :bug: fix: rel='noreferrer' adicionado aos links do contato para solucionar o erro

--- _next/static/CjdX8qCV9c5uQDbFL_fYs/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a7ade9367a5b9196.css","static/chunks/pages/index-a809e1c94c546f73.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
