9078775bad952da71c5e941793c8899fd6717cb9
Rian Ernesto <rianernesto9@gmail.com>
2023-02-02 16:52:52 -0300

    :bug: fix: Target blank adicionado aos links do contato

--- src/components/Home/Contact/index.tsx
+++ src/components/Home/Contact/index.tsx
@@ -21,13 +21,13 @@ export default function Contact() {
                         contato@zenithinova.com.br
                     </Typography>
                 </a>
-                <a href='https://wa.me/5591989661300?text=Olá,%20gostaria%20de%20conversar%20sobre%20os%20projetos%20da%20Zenith%20Inova%20!!!'>
+                <a href='https://wa.me/5591989661300?text=Olá,%20gostaria%20de%20conversar%20sobre%20os%20projetos%20da%20Zenith%20Inova%20!!!' target="_blank">
                     <UilPhone size={32} />
                     <Typography level={6} size="small" fontWeight={100} lineHeight="1.5rem">
                         (91) 98966-1300
                     </Typography>
                 </a>
-                <a href="https://www.google.com.br/maps/place/Cesupa+-+Campus+Alcindo+Cacela+I+(Argo)/@-1.4504419,-48.4770187,19z/data=!4m6!3m5!1s0x92a48c26da0bbc99:0x3e7fb16332282dbd!8m2!3d-1.4502267!4d-48.4773593!16s%2Fg%2F1q2w5t725">
+                <a href="https://www.google.com.br/maps/place/Cesupa+-+Campus+Alcindo+Cacela+I+(Argo)/@-1.4504419,-48.4770187,19z/data=!4m6!3m5!1s0x92a48c26da0bbc99:0x3e7fb16332282dbd!8m2!3d-1.4502267!4d-48.4773593!16s%2Fg%2F1q2w5t725" target="_blank">
                     <UilMapMarker size={32} />
                     <Typography level={6} size="small" fontWeight={100} lineHeight="1.5rem">
                         Av. Alcindo Cacela, 1523 - 3º Andar - Pier 4
