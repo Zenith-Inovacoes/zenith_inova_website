dd40e773892d9c353675707367e0c63808593a2d
RianErnesto <91086261+RianErnesto@users.noreply.github.com>
2023-02-05 21:08:03 +0000

    Build: (ae38f99) :mag: Adição de robots.txt e metas tags

---
+++ _next/static/RKxLtWJ23iamNHhUxskAP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a7ade9367a5b9196.css","static/chunks/pages/index-c77320ca212e4c2c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
