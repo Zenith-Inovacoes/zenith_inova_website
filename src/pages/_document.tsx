499be3bbc8fe4942a19d87afdd8b8fe5423517cc
Rian Ernesto <rianernesto9@gmail.com>
2023-06-28 14:46:14 -0300

    :sparkles: feat: Google Analytics added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,10 +29,24 @@ export default class MyDocument extends Document {
   }
 
   render(): JSX.Element {
-    return(
+    return (
       <Html lang="pt-br">
         <Head>
           <meta charSet='utf-8' />
+
+          {/* Google Tag */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}}',{page_path: window.location.pathname,});
+              `
+            }}>
+
+          </script>
         </Head>
         <body>
           <Main />
